8036d2fd34ffbd0a578e3eca7f827508a724d660
Sam Roeca <samuel.roeca@gmail.com>
2015-11-01 10:51:34 -0500

    Name blueprint 'bugs'

--- app/views/bugs.py
+++ app/views/bugs.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 from .post_template import posts_post_init
 
 BLUEPRINT_NAME = "bugs"
-error = Blueprint(BLUEPRINT_NAME, __name__, url_prefix="/"+BLUEPRINT_NAME)
+bugs = Blueprint(BLUEPRINT_NAME, __name__, url_prefix="/"+BLUEPRINT_NAME)
 posts, post = posts_post_init(error, BLUEPRINT_NAME)
