4ebd8cf2c43ef55aae6599327215a9c56e4b199d
Sam Roeca <samuel.roeca@gmail.com>
2015-11-01 10:55:27 -0500

    Remove old blueprint reference

--- app/flask_extensions/navigation.py
+++ app/flask_extensions/navigation.py
@@ -3,7 +3,6 @@ from flask_nav.elements import Navbar, View
 
 topbar = Navbar('',
         View('Home', 'index.main_index'),
-        View('Present', 'present.posts'),
         View('One tool', 'tools.posts'),
         View('Avoidable bugs', 'bugs.posts'),
         View('Human error', 'error.posts'),
