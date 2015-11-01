e43b36083368c597aff3f0fc1c9ae2756492814a
Sam Roeca <samuel.roeca@gmail.com>
2015-11-01 10:52:04 -0500

    Place bugs in blueprint

--- app/views/bugs.py
+++ app/views/bugs.py
@@ -4,4 +4,4 @@ from .post_template import posts_post_init
 
 BLUEPRINT_NAME = "bugs"
 bugs = Blueprint(BLUEPRINT_NAME, __name__, url_prefix="/"+BLUEPRINT_NAME)
-posts, post = posts_post_init(error, BLUEPRINT_NAME)
+posts, post = posts_post_init(bugs, BLUEPRINT_NAME)
