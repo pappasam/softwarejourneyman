b12ad31ccf838525b802dd1509bf3b254b483b9b
Sam Roeca <samuel.roeca@gmail.com>
2015-11-04 21:40:16 -0500

    Revised views to be coded separately

--- app/views/bugs.py
+++ app/views/bugs.py
@@ -1,7 +1,31 @@
 import os
-from flask import Blueprint
-from .post_template import posts_post_init
+from flask import Blueprint, render_template
+from ..flask_extensions import pages
 
+#######################################################################
+# Initialize blueprint
+#######################################################################
 BLUEPRINT_NAME = "bugs"
 bugs = Blueprint(BLUEPRINT_NAME, __name__, url_prefix="/"+BLUEPRINT_NAME)
-posts, post = posts_post_init(bugs, BLUEPRINT_NAME)
+
+#######################################################################
+# Initialize helpers
+#######################################################################
+SORT_KEY = lambda i: i['date']
+
+#######################################################################
+# Handle views
+#######################################################################
+@bugs.route('/posts/')
+def posts():
+    posts = [p for p in pages if p.path.startswith(BLUEPRINT_NAME)]
+    posts.sort(key=SORT_KEY, reverse=True)
+    template = os.path.join(BLUEPRINT_NAME, 'posts.html')
+    return render_template(template, posts=posts)
+
+@bugs.route('/posts/<name>/')
+def post(name):
+    post_path = os.path.join(BLUEPRINT_NAME, name)
+    post = pages.get_or_404(post_path)
+    template = os.path.join(BLUEPRINT_NAME, 'post.html')
+    return render_template(template, post=post)
